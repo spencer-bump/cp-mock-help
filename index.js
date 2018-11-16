f22935503f4f8b72e491dcf61cee100495a039f6
Spencer Eldred <spencer@konturlabs.com>
2018-11-16 09:37:48 -1000

    rename app.js index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/public'));
-// app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/', function(req, res){
     var path = req.path;
